e799d8e432dd16712a2c017f8f4e3f284e54b1c2
umendra-1999 <112004062+umendra-1999@users.noreply.github.com>
2026-06-03 11:36:39 +0530

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+    <div className='flex gap-10 m-4 ml-96'>
+      <div>
+        <h1>CFX</h1>
+      </div>
+      <div className='ml-18 flex gap-14'>
+    <h1>Solutions</h1>
+    <h1>Feautred Courses</h1>
+    <h1>Pricing</h1>
+    <h1>Contact</h1>
+    </div>
+      </div>
+    </>
+  )
+}
+
+export default page
